06a4d2bb95ee629dc84a7f409ec42bf59777d3a2
anto999 <36578053+anto999@users.noreply.github.com>
2018-07-16 21:52:14 +0200

    Update App.js
    petit changement

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">c'est parti!</h1>
+          <h1 className="App-title">c'est parti pour le checkpoint!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
